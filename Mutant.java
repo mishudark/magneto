1352cb7a0cb6ae4f01c03059733519ae9791574a
mishudark <mishudark@srcmoon.local>
2015-05-26 22:33:23 -0500

    add(file): Mutant.java

---
+++ Mutant.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+
+class Mutant {
+    int dna_len;
+    int counter;
+
+    Mutant(){
+    }
+    public static void main(String[] args){
+      String[] dna = {"ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG"};
+      Mutant mutant = new Mutant();
+      mutant.isMutant(dna);
+    }
+    private boolean isMutant(String[] dna){
+        ArrayList<char[]> array = new ArrayList<>();
+
+        int len;
+        int max_width = 0;
+        int max_height;
+        counter = 0;
+        dna_len = 4;
+        max_height = dna.length;
+
+        //first convert to char[] and get max_length
+        for (String i: dna){
+            array.add(i.toCharArray());
+
+            len = i.length();
+            if(max_width < len){
+                max_width = len;
+            }
+        }
+
+        //fill the matrix
+        char[][] matrix = new char[max_width][max_height];
+        for(int i=0; i<max_height; i++){
+            char[] tmp = array.get(i);
+            check_horizontal(tmp);
+
+            for(int j=0; j<max_width; j++){
+                matrix[i][j] = tmp[j];
+            }
+        }
+
+        System.out.println(counter);
+        return false;
+    }
+
+    private void check_horizontal(char[] item){
+        int l = item.length;
+        char flag;
+        if(l < dna_len){
+            return;
+        }
+
+        for(int i=0; i<l-dna_len+1; i++){
+            flag = item[i];
+            System.out.println("========flag======");
+            System.out.println(flag);
+            System.out.println("-----------------");
+            for(int j=i+dna_len-1; j>i; j--){
+                System.out.println(item[j]);
+                if(flag != item[j]){
+                    break;
+                }else if(j>i+1){
+                  continue;
+                }
+                System.out.println("*******************");
+                System.out.println(flag);
+                System.out.println("*******************");
+                counter++;
+                i += dna_len -1;
+            }
+        }
+    }
+
+}
+
